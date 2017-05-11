6b211ba34abf97aef616e67cb9489478a15236ba
Diego J <diego@intelligenia.com>
2017-05-11 14:17:41 +0200

    Adding models to admin panel

--- src/djshop/apps/store/admin.py
+++ src/djshop/apps/store/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from djshop.apps.store.models import Product, ServingSize
+from djshop.apps.store.models import ProductCategory
+
+admin.register(ProductCategory)
+admin.register(Product)
+admin.register(ServingSize)
